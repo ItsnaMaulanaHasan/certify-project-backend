208f845d42e649eea1e27508e137893c8da496b3
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2024-01-09 17:11:31 +0700

    edit: change port

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, () => console.log("Server running at port 5000"));
+app.listen(5001, () => console.log("Server running at port 5001"));
